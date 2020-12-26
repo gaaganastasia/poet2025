c8009e134336aa65c02ffea742d6af599d0005c1
Anastasia Gaag <gaag.a.a@mail.ru>
2020-12-26 23:07:24 +0700

    random

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const popupWindow = document.querySelector('.popup');
 const startCardButton = document.querySelector('.drum__part_type_button');
 const closeCardButon = document.querySelector('.popup__close');
@@ -34,3 +33,40 @@ closeCardButon.addEventListener('click', (evt) =>{
   remClass(pageDoc, 'page_height');
   closePopupWindow(popupWindow);
 });
+
+////////////
+
+const cardText = document.querySelector('.popup__poem');
+const cardAuthor = document.querySelector('.popup__poem-author');
+const cardTitle = document.querySelector('.popup__poem-title');
+
+function getPoem() {
+  fetch('http://buymebuyme.xyz', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+  .then((res) => {
+    return res.json();
+  }).then((res) => {
+    let rand = Math.random() * (res.length + 1);
+    const randomNumber = Math.floor(rand);
+    const randomPoem = res[randomNumber - 1];
+    console.log(randomPoem);
+    const poemStr = randomPoem.fields.text.split('\n', 4);
+    const poemTitle = randomPoem.fields.text.split('\n', 1);
+    console.log(poemStr);
+    cardText.textContent = poemStr.join('\n');
+    cardTitle.textContent = `${poemTitle}...`;
+    const poemDate = randomPoem.fields.date_from;
+    if (randomPoem.fields.date_to) {
+      cardAuthor.textContent = `${randomPoem.fields.author}, ${poemDate}`
+    }
+    else {
+      cardAuthor.textContent = `${randomPoem.fields.author}`
+    }
+  })
+}
+
+getPoem()
