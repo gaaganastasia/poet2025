d0755d57fdcc137ec5aae2b569362a76b993fe29
Anastasia Gaag <gaag.a.a@mail.ru>
2020-12-26 20:34:00 +0700

    feat: адаптив

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import './pages/index.css';
+
 const popupWindow = document.querySelector('.popup');
 const startCardButton = document.querySelector('.drum__part_type_button');
 const closeCardButon = document.querySelector('.popup__close');
